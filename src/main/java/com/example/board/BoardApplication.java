32bb7cbe25b1a4d09b51fbcadce7a63b17029a84
yui <yuio.s.0731@gmail.com>
2024-10-27 15:57:09 +0900

    boardの全機能とデプロイができる状態のコードを全てコミット

---
+++ src/main/java/com/example/board/BoardApplication.java
@@ -0,0 +1,18 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BoardApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardApplication.class);
+	}
+}
